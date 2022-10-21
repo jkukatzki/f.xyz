aaacf43d903671c2d159087b3e0dd483af4117da
jkukatzki <j.kukatzki@gmail.com>
2022-10-21 22:52:43 +0200

    desk

--- heimseite/src/lib/components/experiences/title/SpinningStuff.svelte
+++ heimseite/src/lib/components/experiences/title/SpinningStuff.svelte
@@ -2,7 +2,6 @@
     import { PerspectiveCamera, PointLight, InstancedMesh, Instance, useThrelte, useFrame, type ThrelteContext} from '@threlte/core';
 	import { useGltf, type ThrelteGltf } from '@threlte/extras';
 
-	import { onMount } from 'svelte/internal';
 	import type { Writable } from 'svelte/store';
     import { SphereGeometry, MeshPhongMaterial, BufferGeometry, Material, Mesh, Vector3, Vector2, BoxGeometry, RGB_PVRTC_2BPPV1_Format} from 'three';
 
@@ -10,53 +9,84 @@
     export let lineStart: Vector3 = new Vector3(0,0,0);
     export let lineEnd: Vector3 = new Vector3(1,0,0);
     export let gltfOrigin: string;
+    export let globalPointer: Vector2 = new Vector2(0.5, 0.5);
+    //TODO: put this in a $: ?
+    let lineStartMirrorX: Vector3 = lineStart.clone().setX(lineStart.x*-1);
+    let lineEndMirrorX: Vector3 = lineEnd.clone().setX(lineEnd.x*-1)
 
     let geoLeft: BufferGeometry;
     let materialLeft: Material;
     let geoRight: BufferGeometry;
     let materialRight: Material;
     let gltf: Writable<ThrelteGltf<any, any> | undefined>;
+    
     $: {
         if(gltfOrigin){console.log('Fetching ', gltfOrigin); ({gltf} = useGltf(gltfOrigin));}
     }
+    $: if($gltf && $gltf.nodes['left'] instanceof Mesh && $gltf.nodes['right'] instanceof Mesh) { geoLeft = $gltf.nodes['left'].geometry; geoRight = $gltf.nodes['right'].geometry; }
     
     const placeholder = new Mesh(new SphereGeometry(0.5, 3, 3), new MeshPhongMaterial({color: 'pink'}));
     [geoLeft, geoRight] =  [placeholder.geometry, placeholder.geometry];
     [materialLeft, materialRight] = [placeholder.material, placeholder.material];
     let time = 0;
-    let pointer = new Vector2(0, 0);
+    
     useFrame((ctx: ThrelteContext) => {
         time = ctx.clock.getElapsedTime();
-        ctx.pointer.subscribe((val) => {
-            pointer = val;
-        })
-    });
-    onMount(() => {
-        gltf.subscribe((val) => {
-            if(!val) return;
-            if(!val.nodes['left'] || !val.nodes['right'] || !(val.nodes['left'] instanceof Mesh) || !(val.nodes['right'] instanceof Mesh)) return;
-            geoLeft = val.nodes['left'].geometry;
-            geoRight = val.nodes['right'].geometry;
-        })
-        
     });
 
+
+    function mix(val: number, toMixBetween: number[]){
+        return (1-val)*toMixBetween[0]+val*toMixBetween[1]
+    }
+
+    function calcInstancePos(i: number): Vector3 {
+        const linePoints = (i < amount ? [lineStartMirrorX.clone(), lineEndMirrorX.clone()] : [lineStart.clone(), lineEnd.clone()]);
+        const indexNormalized = (i % amount);
+        const travel = linePoints[1].sub(linePoints[0]).divideScalar(amount).multiplyScalar(indexNormalized);
+        const pos = linePoints[0].add(travel);
+        return pos;
+    };
+    const rotXLinear = [-1.1, 0.3]
+    function calcInstanceRot(i: number, time: number): Vector3 {
+        const sideX = (i < amount ? 1 : -1);
+        const indexNormalized = (i % amount);
+        return new Vector3(
+            mix(globalPointer.y, rotXLinear),
+            sideX*time*mix(indexNormalized/amount-1, sclLinear),
+            0
+        );
+    }
+    const sclLinear = [1, 0.2];
+    function calcInstanceScl(i: number): Vector3 {
+        const indexNormalized = (i % amount);
+        const scale = mix(indexNormalized/amount-1, sclLinear);
+        return new Vector3(scale, scale, scale);
+    }
 
 </script>
 
-<PerspectiveCamera fov={90} position={{ x: 0, y: 0, z: 0 }}/>
-<PointLight color={'pink'} intensity={2} distance={55} position={{ y: 50 }} />
-<PointLight color={'pink'} intensity={2} distance={55} position={{ y: 0 }} />
+
 {#if $gltf}
     <InstancedMesh id="left" geometry={geoLeft} material={materialLeft}>
         <InstancedMesh id="right" geometry={geoRight} material={materialRight}>
             {#each Array(amount*2) as _, index (index)}
                 <Instance id={index < amount ? 'left' : 'right'}
-                    position={{x: (index < amount ? -1 : 1)*(((index % amount)+1)), y: Math.sin(time), z: (index % amount)+1 * -5}}
-                    rotation={{x: pointer.y, y:(index < amount ? -1 : 1)*time}}
-                    scale={{x:2, y:2, z:2}}
+                    position={calcInstancePos(index)}
+                    rotation={calcInstanceRot(index, time)}
+                    scale={calcInstanceScl(index)}
                 ></Instance>
             {/each}
         </InstancedMesh>
     </InstancedMesh>
+{:else}
+    <InstancedMesh geometry={placeholder.geometry} material={placeholder.material}>
+        {#each Array(amount*2) as _, index (index)}
+            <Instance
+                position={calcInstancePos(index)}
+                rotation={calcInstanceRot(index, time)}
+                scale={calcInstanceScl(index)}
+            ></Instance>
+        {/each}
+    </InstancedMesh>
 {/if}
+
